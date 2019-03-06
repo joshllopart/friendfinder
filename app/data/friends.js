9a668e6841467a611ed531ba0fd1ce5795f593c0
Joshua <joshuag38@gmail.com>
2019-03-06 13:42:04 -0600

    Last push

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,85 +1,85 @@
 var Friends = [
      
-{
+    {
+        
+        name :"Eric",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 5,1,4,4,5,1,2,5,4,1 ]
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    },
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    {
+        
+        name :"John",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 2,1,2,4,5,3,2,1,3,5 ]
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    },
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    {
+        
+        name :"Martha",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 2,4,2,2,2,1,1,3,3,1 ]
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    },
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    {
+        
+        name :"Stephanie",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 4,3,5,2,1,5,5,5,2,5 ]
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    },
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    {
+        
+        name :"Olivia",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 1,1,2,4,2,2,2,3,3,3 ]
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-{
+    },
     
-    name :"Ahmed",
-    photo :"https://picsum.photos/200",
-    scores :[ 5,1,4,4,5,1,2,5,4,1 ]
-
-},
-
-];
-
-module.exports = Friends;
+    {
+        
+        name :"Clarie",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 2,2,4,4,5,2,2,3,3,3 ]
+    
+    },
+    
+    {
+        
+        name :"Ryan",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 5,5,4,4,5,4,5,5,5,3 ]
+    
+    },
+    
+    {
+        
+        name :"Jonathan",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 1,3,4,3,2,2,2,1,2,1 ]
+    
+    },
+    
+    {
+        
+        name :"Katherine",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 2,1,2,2,3,4,2,4,2,1 ]
+    
+    },
+    
+    {
+        
+        name :"Carla",
+        photo :"https://picsum.photos/200/300/?random",
+        scores :[ 5,4,5,5,5,5,5,5,4,3 ]
+    
+    },
+    
+    ];
+    
+    module.exports = Friends;
